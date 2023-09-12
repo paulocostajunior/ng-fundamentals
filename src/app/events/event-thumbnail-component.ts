bd9c3915b33e5076c8257d421256332fc8091fe1
Paulo Costa Jr <costapaulojr@gmail.com>
2023-09-12 09:46:58 +0100

    Using built-in types

--- src/app/events/event-thumbnail-component.ts
+++ src/app/events/event-thumbnail-component.ts
@@ -5,7 +5,7 @@ import { IEvent } from './shared';
     selector: 'event-thumbnail',
     template: `
         <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
+            <h2>{{event.name | uppercase}}</h2>
             <div>Date: {{event.date}}</div>
             <div [ngClass]="getStartTimeClass()" [ngSwitch]="event.time">
                 Time: {{event.time}}
